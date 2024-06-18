9c7b002703062b048c18a5a271dcec247622df72
ANURAG YADAV <anurag_2200816@gniotgroup.edu.in>
2024-06-18 20:01:56 +0530

    first commit

---
+++ src/main/java/com/bookstore/bookstore/Repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.bookstore.bookstore.Repository;
+
+import com.bookstore.bookstore.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book,Long> {
+
+}
---
+++ src/main/java/com/bookstore/bookstore/controller/BookController.java
@@ -0,0 +1,48 @@
+package com.bookstore.bookstore.controller;
+
+import com.bookstore.bookstore.Repository.BookRepository;
+import com.bookstore.bookstore.entity.Book;
+import com.bookstore.bookstore.services.BookService;
+import jakarta.websocket.server.PathParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping(path = "/book")
+public class BookController {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private BookService bookService;
+
+    @PostMapping(path = "/create")
+    public ResponseEntity<Book> createBook(@RequestBody Book book ){
+        return new ResponseEntity<>(bookService.saveBook(book), HttpStatus.CREATED);
+    }
+
+    @GetMapping(path = "/all")
+    public List<Book> getAllBook(){
+        return bookService.getAllBook();
+
+    }
+
+    @PutMapping(path = "/update/{id}")
+    public ResponseEntity<Book> updadeBookById(@PathVariable Long id,@RequestBody Book book){
+        return  new ResponseEntity<>(bookService.updateBookById(id,book),HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping(path = "/delete/{id}")
+    public ResponseEntity<String> deleteById(@PathVariable Long id,@RequestBody Book book){
+            bookService.deleteBookById(id,book);
+        return new ResponseEntity<>("deleted Successfully",HttpStatus.OK);
+    }
+
+
+}
